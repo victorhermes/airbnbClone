c025049795254674829eb229f42b2977ec640c56
Victor Hermes <me.victorhermes@gmail.com>
2019-12-16 11:52:48 -0300

    chore: Recent searches

--- src/pages/BottomTab/ExploreTab/ModalSearch/index.js
+++ src/pages/BottomTab/ExploreTab/ModalSearch/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import Icon from 'react-native-vector-icons/Ionicons';
 import { ScrollView } from 'react-native';
 import {
   Container,
@@ -20,55 +20,55 @@ export default function ModalSearch({ navigation }) {
           <RecentSearches>Buscas Recentes</RecentSearches>
           <Places>
             <IconPlaces>
-              <Icon name="search" size={25} color="#000" />
+              <Icon name="md-time" size={25} color="#000" />
             </IconPlaces>
             <NamePlaces>Penha, Santa Catarina</NamePlaces>
           </Places>
           <Places>
             <IconPlaces>
-              <Icon name="search" size={25} color="#000" />
+              <Icon name="md-time" size={25} color="#000" />
             </IconPlaces>
             <NamePlaces>Piçarras, Santa Catarina</NamePlaces>
           </Places>
           <Places>
             <IconPlaces>
-              <Icon name="search" size={25} color="#000" />
+              <Icon name="md-time" size={25} color="#000" />
             </IconPlaces>
             <NamePlaces>São Paulo, São Paulo</NamePlaces>
           </Places>
           <Places>
             <IconPlaces>
-              <Icon name="search" size={25} color="#000" />
+              <Icon name="md-time" size={25} color="#000" />
             </IconPlaces>
             <NamePlaces>Navegantes, Santa Catarina</NamePlaces>
           </Places>
           <Places>
             <IconPlaces>
-              <Icon name="search" size={25} color="#000" />
+              <Icon name="md-time" size={25} color="#000" />
             </IconPlaces>
             <NamePlaces>Barra Velha, Santa Catarina</NamePlaces>
           </Places>
           <Places>
             <IconPlaces>
-              <Icon name="search" size={25} color="#000" />
+              <Icon name="md-time" size={25} color="#000" />
             </IconPlaces>
             <NamePlaces>BC, Santa Catarina</NamePlaces>
           </Places>
           <Places>
             <IconPlaces>
-              <Icon name="search" size={25} color="#000" />
+              <Icon name="md-time" size={25} color="#000" />
             </IconPlaces>
             <NamePlaces>Santa Catarina</NamePlaces>
           </Places>
           <Places>
             <IconPlaces>
-              <Icon name="search" size={25} color="#000" />
+              <Icon name="md-time" size={25} color="#000" />
             </IconPlaces>
             <NamePlaces>Vila Água Funda</NamePlaces>
           </Places>
           <Places>
             <IconPlaces>
-              <Icon name="search" size={25} color="#000" />
+              <Icon name="md-time" size={25} color="#000" />
             </IconPlaces>
             <NamePlaces>Rio de Janeiro</NamePlaces>
           </Places>
